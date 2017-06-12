2b14ca14a6229e83805bc292c78faed6b803c0fd
coder-god <407410014@qq.com>
2017-06-12 23:40:10 +0800

    添加Ex28

--- Ex28StringPermutation.java
+++ Ex28StringPermutation.java
@@ -1,11 +1,64 @@
 package swordOffer;
 
+import java.util.Scanner;
+
+/**
+ *面试题 28：字符串的排列
+ *题目：输入一个字符串，打印出该字符串中字符的所有排列。
+ *
+ * @author Stephen Huge
+ * @date 2017年6月12日
+ *
+ */
 public class Ex28StringPermutation {
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		
 	}
 	
 	public void StringPermutation(String str) {
+		if(str == null) {
+			return;
+		}
+		int len = str.length();
+		for(int j = 0; j < i; j++) {
+			
+		}
+		for(int i = 0; i < len - 1; i++) {
+			String s = str.substring(0, i) + str.substring(i - 1, i) + str.substring(i, len - 1);
+		}
 		
-	}
+		
+		
+		
+		
+	}*/
+    public static void main(String[] args) {
+//        String str = "";
+//          
+//        Scanner scan = new Scanner(System.in);
+//          
+//        str = scan.nextLine();
+        String str = "abcd";
+        permutation(str.toCharArray(), 0);
+    }
+  
+    public static void permutation(char[] str, int i) {
+        if (i >= str.length)
+            return;
+        if (i == str.length - 1) {
+            System.out.println(String.valueOf(str));
+        } else {
+            for (int j = i; j < str.length; j++) {
+                char temp = str[j];
+                str[j] = str[i];
+                str[i] = temp;
+  
+                permutation(str, i + 1);
+  
+                temp = str[j];
+                str[j] = str[i];
+                str[i] = temp;
+            }
+        }
+    } 
 }
